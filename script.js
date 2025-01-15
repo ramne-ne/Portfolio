16f6f16f31111e932114f80681c7619b74f89bc8
ramne-ne <ramramnerune@gmail.com>
2025-01-15 19:10:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.querySelectorAll('.icons img').forEach(icon => {
-  icon.addEventListener('click', function () {
-      // アイコンの active クラス切り替え
-      document.querySelectorAll('.icons img').forEach(img => img.classList.remove('active'));
-      this.classList.add('active');
+        // アイコン押下時の画面切り替え
+        document.querySelectorAll('.icons img').forEach(icon => {
+          icon.addEventListener('click', function () {
+              // アイコンのactiveクラスを切り替え
+              document.querySelectorAll('.icons img').forEach(img => img.classList.remove('active'));
+              this.classList.add('active');
 
-      // コンテンツの切り替え
-      document.querySelectorAll('.content').forEach(content => content.classList.remove('active'));
-      const target = this.getAttribute('data-target');
-      document.getElementById(target).classList.add('active');
-  });
-});
+              // 表示するswipe-itemを切り替え
+              const targetId = this.getAttribute('data-target');
+              document.querySelectorAll('.swipe-item').forEach(item => item.classList.remove('active'));
+              document.getElementById(targetId).classList.add('active');
+          });
+      });
