46e8d244109de2e782c4951d3a549b9a382ff051
ramne-ne <ramramnerune@gmail.com>
2025-01-17 19:42:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-        // アイコン押下時の画面切り替え
+
+function updateClock() {
+    const clockElement = document.getElementById("clock");
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    clockElement.textContent = `${hours}:${minutes}`;
+}
+
+// 初期表示と1分ごとの更新
+setInterval(updateClock, 60000); // 更新間隔を1分に設定
+updateClock();
+
+
+
+
+
+// アイコン押下時の画面切り替え
         document.querySelectorAll('.icons img').forEach(icon => {
           icon.addEventListener('click', function () {
               // アイコンのactiveクラスを切り替え
@@ -11,3 +28,5 @@
               document.getElementById(targetId).classList.add('active');
           });
       });
+
+      
